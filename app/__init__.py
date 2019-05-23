c269d25654900aedd6bdebb04f3047e1452a7715
yangmingwei <yangmv@yangmv.local>
2019-05-23 16:46:39 +0800

    确保scheduler只运行一次

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,6 @@ from flask import Flask
 from config import config
 from flask_sqlalchemy import SQLAlchemy
 from flask_apscheduler import APScheduler
-import fcntl
-import atexit
 
 db = SQLAlchemy()
 scheduler = APScheduler()
@@ -25,18 +23,9 @@ def create_app(config_name):
     db.init_app(app)
     db.app = app
 
-    # 初始化apscheduler,全局锁确保scheduler只运行一次
-    f = open("scheduler.lock", "wb")
-    try:
-        fcntl.flock(f, fcntl.LOCK_EX | fcntl.LOCK_NB)
-        scheduler.init_app(app)
-        scheduler.start()
-    except:
-        pass
-    def unlock():
-        fcntl.flock(f, fcntl.LOCK_UN)
-        f.close()
-    atexit.register(unlock)
+    # 初始化apscheduler
+    scheduler.init_app(app)
+    scheduler.start()
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
